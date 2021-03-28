bd573145808fa22631a995ba69845c598ae30780
Ryojidev <ryojikitanodev@gmail.com>
2021-03-28 18:20:45 -0300

    inicio implementação Todo Frontend, criado comopanentes,menu,Todo e adicionado rotas

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props =>(
+  <div>
+    <PageHeader name='Sobre' small='Nós' />
+    <h2>Nossa historia</h2>
+    <p>Fazendo aplicativos para melhorar a gestão de tarefas</p>
+    <h2>Misão e Visão</h2>
+    <p>Ajudamos a gerenciar tarefas</p>
+  </div>
+)
