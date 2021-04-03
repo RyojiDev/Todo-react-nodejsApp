76e42515ae794a243bb9e6d827217d57e1bc2201
Ryojidev <ryojikitanodev@gmail.com>
2021-04-03 15:58:42 -0300

    removendo imports que não estão mais sendo utilizados apos a migração

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import axios from 'axios';
 
 import PageHeader from '../template/pageHeader';
 import TodoForm from '../todo/todoForm';
 import TodoList from '../todo/todoList';
 
-const URL = 'http://localhost:3003/api/todos'
-
-let list = [{_id: 1, description: 'fdfdfdsfd'}, {_id: 2, description: 'teste'}]
 export default class Todo extends React.Component{
 
   render(){
